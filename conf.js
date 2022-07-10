c94405cefef3a3ac1e0f880ddd33d17e08961f68
SuperUser123677 <65298427+SuperUser123677@users.noreply.github.com>
2022-07-10 08:01:20 -0400

    Update conf.js

--- conf.js
+++ conf.js
@@ -89,7 +89,7 @@ jsproxy_config({
       replace: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'
     },
     'https://www.pornhub.com/': {
-      redir: 'https://blog.51sec.org/'
+      redir: 'https://apps.securedserveraccess.com/'
     },
     'http://haha.com/': {
       content: 'Hello World'
